efe4dcf9655628af600f42c438970d9fd7e72bf6
Alistair Smith <alistairsmith@flightofstairs.org>
2015-07-12 16:57:47 +0100

    Computing days of week.

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -1,7 +1,7 @@
 angular.module('SettingsCtrl', []).controller('SettingsCtrl', function($scope, $localStorage) {
     $scope.options = {
         hours: _.range(0, 24),
-        days: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+        days: _.range(0, 7).map(function(i) { return moment().weekday(i + 1).format("dddd").toLowerCase(); })
     };
 
     $scope.$storage = $localStorage;
