3a0f6b630d21613d8e8c8c0d942ec855fe5c2788
Alistair Smith <alistairsmith@flightofstairs.org>
2015-07-12 18:49:52 +0100

    Added day-off message.

--- www/js/controllers/today.js
+++ www/js/controllers/today.js
@@ -1,3 +1,10 @@
 angular.module('TodayCtrl', []).controller('TodayCtrl', function($scope, $localStorage) {
 
+    $scope.isDayOff = function() {
+        return ! $localStorage.settings.days[moment().format("dddd").toLowerCase()];
+    };
+
+    $scope.record = function() {
+        return $localStorage.log[moment().format("YYYY-MM-DD")];
+    };
 });
