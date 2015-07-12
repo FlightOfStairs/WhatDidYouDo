32786249fe1fb50cb93c23a77e889f2c00909441
Alistair Smith <alistairsmith@flightofstairs.org>
2015-07-12 20:17:26 +0100

    Some work on 'today' tab.

--- www/js/controllers/today.js
+++ www/js/controllers/today.js
@@ -1,10 +1,35 @@
 angular.module('TodayCtrl', []).controller('TodayCtrl', function($scope, $localStorage) {
+    var dateString = moment().format("YYYY-MM-DD");
 
-    $scope.isDayOff = function() {
-        return ! $localStorage.settings.days[moment().format("dddd").toLowerCase()];
-    };
+    if (! $localStorage.log[dateString]) {
+        $localStorage.log[dateString] = { };
+    }
+    var dayRecord = $localStorage.log[dateString];
 
-    $scope.record = function() {
-        return $localStorage.log[moment().format("YYYY-MM-DD")];
-    };
+    $scope.isDayOff = ! $localStorage.settings.days[moment().format("dddd").toLowerCase()];
+
+    $scope.records = (function() {
+        var start = $localStorage.settings.hours.start;
+        var end = Math.min($localStorage.settings.hours.end, moment().hour());
+
+        if ($scope.showAll) end = $localStorage.settings.hours.end;
+
+        return _.range(start, end).map(function(hour) {
+            if (! dayRecord[hour]) {
+                dayRecord[hour] = {
+                    notes: "adsfas",
+                    tags: ["Meetings", "Pointless"],
+                    completed: false
+                }
+            }
+            var record = dayRecord[hour];
+            record.hour = hour;
+
+            return record;
+        });
+    })();
+
+    $scope.toggleComplete = function(hour) {
+        dayRecord[hour].completed = ! dayRecord[hour].completed;
+    }
 });
